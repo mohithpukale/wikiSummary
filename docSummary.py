1f05d258aa42096b15860a7ec68bf62c6bb48232
Ashwini <ap1995@users.noreply.github.com>
2018-03-11 13:15:34 -0700

    Updated to give text summary
    TODO: remove extra characters

--- docSummary.py
+++ docSummary.py
@@ -1,63 +1,84 @@
-import sklearn
-import numpy
-import scipy
+import functools
+from nltk.probability import FreqDist
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 import nltk.data
 import re
 
-def clean(string):
 
+def clean(string):
     string = re.sub(r"\'s", " \'s", string)
     string = re.sub(r"\'ve", " \'ve", string)
     string = re.sub(r"n\'t", " n\'t", string)
     string = re.sub(r"\'re", " \'re", string)
     string = re.sub(r"\'d", " \'d", string)
     string = re.sub(r"\'ll", " \'ll", string)
-    # string = re.sub(r",", " , ", string)
     string = re.sub(r"!", " ! ", string)
     string = re.sub(r"\(", " \( ", string)
     string = re.sub(r"\)", " \) ", string)
     string = re.sub(r"\?", " \? ", string)
-    string = re.sub(r"\s{2,}", " ", string)
+    string = re.sub(r"\n", "", string)
+    string = string.rstrip()
     string = remove_text_inside_brackets(string)
-    # string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
     return string.strip()
 
-def remove_text_inside_brackets(text, brackets="[]"):
-    count = [0] * (len(brackets) // 2) # count open/close brackets
+def remove_text_inside_brackets(text, brackets="(){}[]"):
+    count = [0] * (len(brackets) // 2)  # count open/close brackets
     saved_chars = []
     for character in text:
         for i, b in enumerate(brackets):
-            if character == b: # found bracket
+            if character == b:  # found bracket
                 kind, is_close = divmod(i, 2)
-                count[kind] += (-1)**is_close # `+1`: open, `-1`: close
-                if count[kind] < 0: # unbalanced bracket
+                count[kind] += (-1) ** is_close  # `+1`: open, `-1`: close
+                if count[kind] < 0:  # unbalanced bracket
                     count[kind] = 0  # keep it
                 else:  # found bracket to remove
                     break
-        else: # character is not a [balanced] bracket
-            if not any(count): # outside brackets
+        else:  # character is not a [balanced] bracket
+            if not any(count):  # outside brackets
                 saved_chars.append(character)
     return ''.join(saved_chars)
 
-def main():
-    input = 'The English Wikipedia reached 4,000,000 registered user accounts on 1 April 2007,[15] ' \
-            'just a little over a year since it had crossed a threshold of 1,000,000 registered user accounts in late ' \
-            'February 2006.[16] Over 800,000 editors have edited Wikipedia more than 10 times.[17] 300,000 editors edit ' \
-            'Wikipedia every month[citation needed]; of these, over 30,000 perform more than 5 edits per month, and a little' \
-            ' over 3,000 perform more than 100 edits per month.[18] By 24 November 2011, a total of 500 million edits ' \
-            'had been performed on the English Wikipedia.[citation needed] As the largest Wikipedia edition, and because ' \
-            'English is such a widely used language, the English Wikipedia draws many users and editors whose native ' \
-            'language is not English. Such users may seek information from the English Wikipedia rather than the Wikipedia ' \
-            'of their native language because the English Wikipedia tends to contain more information about general ' \
-            'subjects. Successful collaborations have been developed between non-native English speakers who successfully ' \
-            'add content to the English Wikipedia and native English speakers who act as copyeditors for them.'
 
+def reorder_sentences(output_sentences, input):
+    def custom_sort(s1, s2):
+        return input.find(s1) - input.find(s2)
+
+    output_sentences.sort(key=functools.cmp_to_key(custom_sort))
+    return output_sentences
+
+def get_summarized(input, num_sentences):
+
+    tokenizer = RegexpTokenizer('\w+')
+    base_words = [word.lower() for word in tokenizer.tokenize(input)]
+    words = [word for word in base_words if word not in stopwords.words()]
+    word_frequencies = FreqDist(words)
+    most_frequent_words = [pair[0] for pair in word_frequencies.most_common(100)
+
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    actual_sentences = sent_detector.tokenize(input)
+    working_sentences = [sentence.lower() for sentence in actual_sentences]
+    output_sentences = []
+
+    for word in most_frequent_words:
+        for i in range(0, len(working_sentences)):
+            if (word in working_sentences[i] and actual_sentences[i] not in output_sentences):
+                output_sentences.append(actual_sentences[i])
+                break
+            if len(output_sentences) >= num_sentences: break
+        if len(output_sentences) >= num_sentences: break
+                           
+    return reorder_sentences(output_sentences, input)
+
+def summarize(input, num_sentences):
+    return " ".join(get_summarized(input, num_sentences))
+
+
+def main():
+    f = open('plaintext.txt', 'r')
+    input = f.read()
     result = clean(input)
-    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-    print('\n'.join(tokenizer.tokenize(result)))
-    
+    summary = summarize(result, 5)
+    print(summary)
 
 main()
-
-
-
