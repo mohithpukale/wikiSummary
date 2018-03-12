7f7941bafe5763360ecb4235008698c69fd3ad34
Mohith Pukale <mohithrp93@gmail.com>
2018-03-12 11:22:53 -0700

    Adding index page and restructuring code

---
+++ app/docSummary.py
@@ -0,0 +1,80 @@
+import functools
+import re
+
+import nltk.data
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+from nltk.tokenize import RegexpTokenizer
+
+
+def clean(string):
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\n", "", string)
+    string = string.rstrip()
+    string = remove_text_inside_brackets(string)
+    return string.strip()
+
+
+def remove_text_inside_brackets(text, brackets="(){}[]"):
+    count = [0] * (len(brackets) // 2)  # count open/close brackets
+    saved_chars = []
+    for character in text:
+        for i, b in enumerate(brackets):
+            if character == b:  # found bracket
+                kind, is_close = divmod(i, 2)
+                count[kind] += (-1) ** is_close  # `+1`: open, `-1`: close
+                if count[kind] < 0:  # unbalanced bracket
+                    count[kind] = 0  # keep it
+                else:  # found bracket to remove
+                    break
+        else:  # character is not a [balanced] bracket
+            if not any(count):  # outside brackets
+                saved_chars.append(character)
+    return ''.join(saved_chars)
+
+
+def reorder_sentences(output_sentences, input):
+    def custom_sort(s1, s2):
+        return input.find(s1) - input.find(s2)
+
+    output_sentences.sort(key=functools.cmp_to_key(custom_sort))
+    return output_sentences
+
+
+def get_summarized(input, num_sentences):
+    tokenizer = RegexpTokenizer('\w+')
+    base_words = [word.lower() for word in tokenizer.tokenize(input)]
+    words = [word for word in base_words if word not in stopwords.words()]
+    word_frequencies = FreqDist(words)
+    most_frequent_words = [pair[0] for pair in word_frequencies.most_common(100)]
+
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    actual_sentences = sent_detector.tokenize(input)
+    working_sentences = [sentence.lower() for sentence in actual_sentences]
+    output_sentences = []
+
+    for word in most_frequent_words:
+        for i in range(0, len(working_sentences)):
+            if word in working_sentences[i] and actual_sentences[i] not in output_sentences:
+                output_sentences.append(actual_sentences[i])
+                break
+            if len(output_sentences) >= num_sentences:
+                break
+
+            if len(output_sentences) >= num_sentences:
+                break
+
+    return reorder_sentences(output_sentences, input)
+
+
+def summarize(input, num_sentences):
+    return " ".join(get_summarized(input, num_sentences))
---
+++ app/views.py
@@ -0,0 +1,64 @@
+from elasticsearch2 import Elasticsearch
+from flask import jsonify
+from flask import render_template
+from flask import request
+
+from app import app
+from app import docSummary
+
+es = Elasticsearch(['https://73efa8624ce5b1aa7b0636a629e2d9f1.us-west-1.aws.found.io:9243/'],
+                   http_auth=('admin', 'jfnN6ArBrfnlD6accc0WatAy'),
+                   scheme="https")
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', name='index')
+
+
+@app.route('/query/', methods=['GET'])
+def query():
+    search_word = request.args.get('q')
+    term = {
+        "query": {
+            "bool": {
+                "must_not": [
+                    {
+                        "match": {
+                            "text": "#REDIRECT"
+                        }
+                    }
+                ],
+                "must": [
+                    {
+                        "match_phrase": {
+                            "text": search_word
+                        }
+                    }
+                ]
+            }
+        },
+        "size": 15,
+        "_source": [
+            "text",
+            "title"
+        ]
+    }
+    res = es.search(index="wiki", body=term)
+
+    print("Got %d Hits:" % res['hits']['total'])
+    for hit in res['hits']['hits']:
+        # temp = hit['_source']['text']
+        result = docSummary.clean(hit['_source']['text'])
+        summary = docSummary.summarize(result, 5)
+        hit['_source']['text'] = summary
+
+    '''
+    print('Original String\n\n')
+    print(temp)
+    print('Cleaned String\n\n')
+    print(result)
+    print('Summarized String\n\n')
+    print(summary)'''
+    return jsonify({'results': res['hits']})
